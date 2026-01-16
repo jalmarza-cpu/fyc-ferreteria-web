3b84d60997629cda389737470d9aa87a0f06bc3e
jalmarza-cpu <jalmarza@kirisinmobiliaria.com>
2026-01-16 02:01:09 -0300

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -32,7 +32,7 @@ export const PRODUCTS: Product[] = [
     description: "Brocha 4″.",
     priceRetail: 1900,
     priceWholesale: 1620,
-    imageUrl: "/011387-brocha-forceman-4.png",
+    imageUrl: "/011387-brocha-forceman-4.jpg",
     category: "Brocha, Herramientas"
   },
   {
